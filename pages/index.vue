c80f8125f71bae7d39f01313f9a23780b1b9148b
yosong <2404559603@qq.com>
2024-05-11 16:26:45 +0800

    'x'

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
  * @Author: yosong
  * @Date: 2024-05-09 11:29:43
  * @LastEditors: Do not edit
- * @LastEditTime: 2024-05-11 14:52:52
+ * @LastEditTime: 2024-05-11 16:26:32
  * @FilePath: \blog\pages\index.vue
 -->
 <template>
@@ -14,7 +14,7 @@
       class="object-cover rounded-full w-102px h-102px m-auto" />
     <h1 data-aos="fade-down" class="font-size-26px text-center my-8">yosong</h1>
     <p data-aos="fade-down" class="my-6 opacity-50">
-      I'm yosong and I'm a front-end developer
+      I'm yosong, a front-end developer
     </p>
     <div class="flex-center">
       <img
