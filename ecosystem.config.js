4b186c0fc9565eac32e84679ab69cb9e494236f5
yosong <2404559603@qq.com>
2024-10-16 13:22:52 +0800

    pm2部署文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'yosong_blog', // 应用程序名称
-      port: '80http://139.9.32.77:7111/', // 监听端口
+      port: '7111', // 监听端口
       args: '', // 传递给脚本的参数
       instances: 'max', // 应用实例数
       autorestart: true, // 自动重启
